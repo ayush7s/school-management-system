eb7b00f1f26d3abf7220042d8bc72687bbc7faff
Ayush Das <dasayush9134@gmail.com>
2026-01-28 02:04:30 +0530

    Add database models

--- models/announcement.py
+++ models/announcement.py
@@ -4,6 +4,9 @@ from models.user import db
 class Announcement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=True) 
+
+    file_name = db.Column(db.String(255), nullable=True)  # ✅ FILE NAME
+   
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
