144ab93a860ebe50f099673f67c1bc6db8ffcb1c
Ayush Das <dasayush9134@gmail.com>
2026-01-28 02:06:15 +0530

    Update database initialization logic

--- init_db.py
+++ init_db.py
@@ -4,7 +4,7 @@ from models.notice import Notice
 from models.study_material import StudyMaterial
 from models.user import User
 from models.announcement import Announcement
-
+from models.paid_material import PaidMaterial
 with app.app_context():
     db.create_all()
     print("Database created successfully")
